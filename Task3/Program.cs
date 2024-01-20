95e0f82166b735705d9b136be53b4d456a4fe8d9
Sultan Abdullaev <sultan230199@yandex.ru>
2024-01-20 15:54:06 +0300

    Added lesson2/task3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Нахождение максимума
+int [] array = {19,20,3,16,8,4};
+int i =0;
+int max =array[0];
+
+while (i< array.Length)
+{
+    if (max<array[i])
+    {
+        max = array[i];
+    }
+    i++;
+}
+Console.Write(max);
